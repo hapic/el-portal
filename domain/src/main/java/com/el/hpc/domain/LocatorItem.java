a1329a3fabc96005ffdf263cd0c0d0c1f699d497
ShenQi <hapic@163.com>
2017-04-28 22:27:24 +0800

    删除多余样式

---
+++ domain/src/main/java/com/el/hpc/domain/LocatorItem.java
@@ -0,0 +1,23 @@
+package com.el.hpc.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * Created by ShenQi on 17/4/28.
+ */
+@Setter
+@Getter
+public class LocatorItem implements Serializable {
+    private Integer id;
+    private String name;
+    private String desc;
+    private String href;
+    private String isNew;
+    private String host;
+
+
+}
